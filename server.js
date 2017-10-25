2caa7fb8cbf64483c55c15ae605340d4ebf8d3b6
Ebonie Lumpkin <ebonie.b.lumpkin@gmail.com>
2017-10-25 01:08:23 -0500

    commiting addtions to code to make reusable with ejs

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
-
-
-
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var app =  express();
-var port = 8090;
+var port = 8070;
+
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 //route our app
 var router = require('./app/routes');
 app.use('/',router);
+
 
 //set static files
 app.use(express.static(__dirname + '/public'));
